0b69620c080b3c5a9b9a891ad73e0dca2d811c6b
Scoutamass <42789432+Scoutamass@users.noreply.github.com>
2024-09-24 14:42:18 -0400

    Update memory-user.c

--- memory-user.c
+++ memory-user.c
@@ -4,7 +4,9 @@
 
 int main(int argc, char* argv[])
 {
-  if(argc < 2) return;
-  malloc(argv[1]);
-  sleep(10);
+  if(argc < 2) return -1;
+  int* mem = malloc(atoi(argv[1])*1024*1024);
+  for(int i = 0; i < 100000000; i++) for(int j = 0; j < atoi(argv[1])*1024*1024/sizeof(int); j++) mem[j];
+  free(mem);
+  return 0;
 }
